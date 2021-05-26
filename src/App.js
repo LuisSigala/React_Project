df444e1d85e5806e05b790f24a59897bf9b6a85b
LuisSigala <a6519150031@utch.edu.mx>
2021-05-26 17:35:24 -0600

    Sign in link and form

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import{ BrowserRouter, Route, Switch} from "react-router-dom"
 import Layout from "./pages/layout/Layout"
 import Main from "./pages/main/Main"
+import Sign_In from "./pages/sign_in/Sign_In"
 import NewBadge from "./pages/NewBadge/NewBadge"
 
 
@@ -12,6 +13,7 @@ function App() {
         <Switch>
           <Route exact path="/" component={Main}></Route>
           <Route exact path="/new" component={NewBadge}></Route>
+          <Route exact path="/Sign_in" component={Sign_In}></Route>
           <Route component={Error}></Route>
         </Switch>
       </Layout>
