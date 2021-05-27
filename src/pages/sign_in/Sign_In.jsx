d8d08d5ccec6ae71032e0ca0ffe89f93aefc5c62
LuisSigala <a6519150031@utch.edu.mx>
2021-05-26 18:32:11 -0600

    Sign in css config

--- src/pages/sign_in/Sign_In.jsx
+++ src/pages/sign_in/Sign_In.jsx
@@ -5,23 +5,23 @@ import "./Sign_In.css"
 class Sign_In extends React.Component{
     render(){
         return(
-            <React.Fragment>                
-                <div className="container">
+            <React.Fragment>
+                <center>            
+                <div className="contain">
                         <div class="form-group">
                             <label for="exampleInputEmail1">Email address</label>
                             <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-                            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                            <small id="emailHelp" class="form-text text-muted"></small>
                         </div>
                         <div class="form-group">
                             <label for="exampleInputPassword1">Password</label>
                             <input type="password" class="form-control" id="exampleInputPassword1"/>
                         </div>
-                        <div class="form-group form-check">
-                            <input type="checkbox" class="form-check-input" id="exampleCheck1"/>
-                            <label class="form-check-label" for="exampleCheck1">Check me out</label>
-                        </div>
-                        <button type="submit" class="btn btn-primary">Submit</button>
+                        <center>
+                        <button type="submit" class="btn btn-primary m-2 col-7">Submit</button>
+                        </center>
                 </div>
+                </center> 
             </React.Fragment>
         );
     }
