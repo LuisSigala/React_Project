42c136147518f1e50fe15fc6fa2956d35da65503
LuisSigala <a6519150031@utch.edu.mx>
2021-06-08 16:05:18 -0600

    src/pages/Badges/ configurations

---
+++ src/pages/Badges/Badges.jsx
@@ -0,0 +1,54 @@
+import React from "react"
+import SkeletonItem from "../../components/SkeletonItem"
+import BadgeList from ""
+import api from "../../libs/api"
+import "./Badges.css"
+import BadgesList from "../../components/BabgeList"
+
+class Badges extends React.Component{
+
+    state = {
+        loading:true,
+        error:null,
+        data:undefined
+    }
+
+    componentDidMount(){
+        this.fetchData();
+        this.setFetchInterval();
+    }
+
+    fetchData = async() =>{
+        this.setState({loading: true, error: null})
+        try{
+            const data = await api.badges.list();
+            data.reverse()
+            this.setState({loading: false, data: data})
+        }catch(error){
+            this.setState({loading: false, error: error, data: []})
+        }
+    }
+
+    setFetchInterval(){
+        this.interval = setInterval(this.fetchData, 3000)
+    }
+
+    componentWillUnmount(){
+        clearInterval(this.interval)
+    }
+
+    render(){
+        if(this.state.loading==true && !this.state.data){
+            return <SkeletonItem></SkeletonItem>
+        }
+
+        return(
+            <React.Fragment>
+                <div className="Badges__container"></div>
+                <BadgesList badges={this.state.data}></BadgesList>
+            </React.Fragment>
+        );
+    }
+}
+
+export default Badges;
