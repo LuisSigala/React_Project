572e8cb2693bb4c9b404759934d736fc25fb9c63
LuisSigala <a6519150031@utch.edu.mx>
2021-06-08 16:00:25 -0600

    App.js configurations

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Sign_In from "./pages/sign_in/Sign_In"
 import Login from "./pages/login/Login"
 import Not_Found from "./pages/not_found/Not_Found"
 import NewBadge from "./pages/NewBadge/NewBadge"
+import Badges from "./pages/Badges/Badges"
 
 
 function App() {
@@ -15,10 +16,10 @@ function App() {
         <Switch>
           <Route exact path="/" component={Main}></Route>
           <Route exact path="/new" component={NewBadge}></Route>
+          <Route exact path="/badges" component={Badges}></Route>
           <Route exact path="/Sign_in" component={Sign_In}></Route>
           <Route exact path="/Login" component={Login}></Route>
-          <Route exact path="/Error" component={Not_Found}></Route>
-          <Route component={Error}></Route>
+          <Route component={Not_Found}></Route>
         </Switch>
       </Layout>
     </BrowserRouter>
